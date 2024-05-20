e4a02f6830fcbf130b59d06ae4142cecca2b7313
Lord-binns <vince.olemberio2004@gmail.com>
2024-05-20 17:06:23 +0800

    edited dashboard.php

--- admin/public/dashboard.php
+++ admin/public/dashboard.php
@@ -1,14 +1,26 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: ../index.php");
+    exit;
+}
+?>
+ 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Dashboard</title>
+    <title>Welcome</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <style>
+        body{ font: 14px sans-serif; }
         .wrapper{
             width: 600px;
             margin: 0 auto;
@@ -17,70 +29,67 @@
             width: 120px;
         }
     </style>
-    <script>
-        $(document).ready(function(){
-            $('[data-toggle="tooltip"]').tooltip();   
-        });
-    </script>
 </head>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand"  href="dashboard.php">Admin</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="dashboard.php">Dashboard <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="products.php">Products</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+     <!--<a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>-->
+        <a href="../public/user/logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+    </form>
+  </div>
+</nav>
 <body>
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="mt-5 mb-3 clearfix">
-                        <h2 class="pull-left">Product Details</h2>
-                        <a href="./public/create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Product</a>
-                    </div>
-                    <?php
-                    // Include config file
-                    require_once "db/config.php";
+
+<div class="container-fluid" >
+    <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+        <div class="card-header" href="products.php">Products Recorded</div>
+        <div class="card-body">
+            <h5 class="card-title "></h5>
+            
+            <?php
+            // Include config file
+            require_once "../db/config.php";
+            
+            // Attempt select query execution
+            $sql = "SELECT COUNT(*) AS total_products FROM products";
+            if($stmt = $pdo->prepare($sql)){
+                // Attempt to execute the prepared statement
+                if($stmt->execute()){
+                    // Bind result variables
+                    $stmt->bindColumn('total_products', $total_products);
                     
-                    // Attempt select query execution
-                    $sql = "SELECT * FROM products";
-                    if($result = $pdo->query($sql)){
-                        if($result->rowCount() > 0){
-                            echo '<table class="table table-bordered table-striped">';
-                                echo "<thead>";
-                                    echo "<tr>";
-                                        echo "<th>Product_id</th>";
-                                        echo "<th>Product_Name</th>";
-                                        echo "<th>Product_Description</th>";
-                                        echo "<th>Product_Retail_Price</th>";
-                                        echo "<th>Action</th>";
-                                    echo "</tr>";
-                                echo "</thead>";
-                                echo "<tbody>";
-                                while($row = $result->fetch()){
-                                    echo "<tr>";
-                                        echo "<td>" . $row['product_id'] . "</td>";
-                                        echo "<td>" . $row['product_name'] . "</td>";
-                                        echo "<td>" . $row['product_description'] . "</td>";
-                                        echo "<td>" . $row['product_retail_price'] . "</td>";
-                                        echo "<td>";
-                                            echo '<a href="./public/read.php?product_id='. $row['product_id'] .'" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
-                                            echo '<a href="./public/update.php?product_id='. $row['product_id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
-                                            echo '<a href="./public/delete.php?product_id='. $row['product_id'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
-                                        echo "</td>";
-                                    echo "</tr>";
-                                }
-                                echo "</tbody>";                            
-                            echo "</table>";
-                            // Free result set
-                            unset($result);
-                        } else{
-                            echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
-                        }
+                    // Fetch result
+                    if($stmt->fetch()){
+                        echo '<h1 class="text-center">' . $total_products . '</h1><br>';
                     } else{
-                        echo "Oops! Something went wrong. Please try again later.";
+                        echo '<p>No products found.</p>';
                     }
-                    
-                    // Close connection
-                    unset($pdo);
-                    ?>
-                </div>
-            </div>        
+                } else{
+                    echo "Oops! Something went wrong. Please try again later.";
+                }
+            }
+            // Close statement
+            unset($stmt);
+            ?>
+            
+            <p class="card-text">Date and Time: <?php echo date("Y-m-d H:i:s"); ?></p>
         </div>
     </div>
+</div>
 </body>
 </html>
--- admin/public/inventory/create.php
+++ admin/public/inventory/create.php
@@ -1,6 +1,6 @@
 <?php
 // Include config file
-require_once $_SERVER['DOCUMENT_ROOT'] . "/olemberio-pdo-crud/db/config.php";
+require_once '../../db/config.php';
 
 // Define variables and initialize with empty values
 $title = $description = $price = $rrp = $quantity = $img = "";
