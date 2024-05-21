a5b438c1b7a95f688d91bdc1cad47c591ec3826b
Lord-binns <vince.olemberio2004gmail.com>
2024-05-21 19:51:58 +0800

    added css and designs to admin products.php

--- admin/public/products.php
+++ admin/public/products.php
@@ -20,10 +20,39 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <style>
-        body{ font: 14px sans-serif; text-align: center; }
-     
-        table tr td:last-child{
-            width: 120px;
+        body {
+            font: 14px sans-serif;
+            text-align: center;
+        }
+        .table-wrapper {
+            margin: 20px 0;
+            background: #fff;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 10px;
+        }
+        .table thead th {
+            background-color: #343a40;
+            color: #fff;
+            border: none;
+        }
+        .table tbody tr:nth-child(odd) {
+            background-color: #f2f2f2;
+        }
+        .table tbody tr:hover {
+            background-color: #e9ecef;
+        }
+        .table tbody td, .table tbody th {
+            vertical-align: middle;
+            text-align: center;
+        }
+        .table tbody td img {
+            width: 50px;
+            height: 50px;
+            object-fit: cover;
+        }
+        .btn-success, .btn-danger {
+            border-radius: 20px;
         }
     </style>
 </head>
@@ -51,107 +80,106 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   </div>
 </nav>
 <body>
-
     <div class="wrapper">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="mt-5 mb-3 clearfix">
-                    <h2 class="pull-left">Product Details</h2>
-                    <a href="./inventory/create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Product</a>
-                </div>
-                <?php
-                // Include config file
-                require_once "../db/config.php";
-                
-                // Attempt select query execution
-                $sql = "SELECT * FROM products";
-                if($result = $pdo->query($sql)){
-                    $totalRows = $result->rowCount();
-
-                    if($result->rowCount() > 0){
-                        // Define the table template
-                        $tableTemplate = '
-                                <table class="table table-bordered table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th> <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                                        </th>
-                                        <th class="text-center" colspan="8"><h6>Showing ' . $totalRows . ' / ' . $totalRows . ' Records</h6></th>
-
-                                        </tr>
-                                    </thead>                               
-                                </table>
-                              
-                                <table class="table table-bordered table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th>Record Number</th>
-                                            <th>Product Name</th>
-                                            <th>Product Description</th>
-                                            <th>Price</th>
-                                            <th>Recommended Retail Price</th>
-                                            <th>Quantity</th>
-                                            <th>Image</th>
-                                            <th>Date Added</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {{rows}}
-                                    </tbody>
-                                </table>
-                   
-                        ';
-                
-                        // Define the row template
-                        $rowTemplate = '
-                            <tr>
-                                <td>{{id}}</td>
-                                <td>{{title}}</td>
-                                <td>{{description}}</td>
-                                <td>{{price}}</td>
-                                <td>{{rrp}}</td>
-                                <td>{{quantity}}</td>
-                                <td>{{img}}</td>
-                                <td>{{date_added}}</td>
-                                <td>
-                                    <a href="./inventory/read.php?id={{id}}" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>
-                                    <a href="./inventory/update.php?id={{id}}" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>
-                                    <a href="./inventory/delete.php?id={{id}}" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>
-                                </td>
-                            </tr>
-                        ';
-                
-                        // Populate the rows using the row template
-                        $rows = '';
-                        while ($row = $result->fetch()) {
-                            $rowHtml = str_replace(
-                                array('{{id}}', '{{title}}', '{{description}}', '{{price}}', '{{rrp}}', '{{quantity}}', '{{img}}', '{{date_added}}'),
-                                array($row['id'], $row['title'], $row['description'], $row['price'], $row['rrp'], $row['quantity'], $row['img'], $row['date_added']),
-                                $rowTemplate
-                            );
-                            $rows .= $rowHtml;
-                        }
-                
-                        // Replace the rows placeholder in the table template with the actual rows
-                        echo str_replace('{{rows}}', $rows, $tableTemplate);
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="table-wrapper">
+                        <div class="mt-5 mb-3 clearfix">
+                            <h2 class="pull-left">Product Details</h2>
+                            <a href="./inventory/create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Product</a>
+                        </div>
+                        <?php
+                        // Include config file
+                        require_once "../db/config.php";
                         
-                        // Free result set
-                        unset($result);
-                    } else{
-                        echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
-                    }
-                } else{
-                    echo "Oops! Something went wrong. Please try again later.";
-                }
-                
-                // Close connection
-                unset($pdo);
-                ?>
-            </div>
-        </div>        
-    </div>
-    </div>
+                        // Attempt select query execution
+                        $sql = "SELECT * FROM products";
+                        if($result = $pdo->query($sql)){
+                            $totalRows = $result->rowCount();
 
+                            if($result->rowCount() > 0){
+                                // Define the table template
+                                $tableTemplate = '
+                                        <table class="table table-bordered table-striped">
+                                            <thead>
+                                                <tr>
+                                                    <th> <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                                                </th>
+                                                <th class="text-center" colspan="8"><h6>Showing ' . $totalRows . ' / ' . $totalRows . ' Records</h6></th>
+
+                                                </tr>
+                                            </thead>                               
+                                        </table>
+                                      
+                                        <table class="table table-bordered table-striped">
+                                            <thead>
+                                                <tr>
+                                                    <th>Record Number</th>
+                                                    <th>Product Name</th>
+                                                    <th>Product Description</th>
+                                                    <th>Price</th>
+                                                    <th>Recommended Retail Price</th>
+                                                    <th>Quantity</th>
+                                                    <th>Image</th>
+                                                    <th>Date Added</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {{rows}}
+                                            </tbody>
+                                        </table>
+                                ';
+                        
+                                // Define the row template
+                                $rowTemplate = '
+                                    <tr>
+                                        <td>{{id}}</td>
+                                        <td>{{title}}</td>
+                                        <td>{{description}}</td>
+                                        <td>{{price}}</td>
+                                        <td>{{rrp}}</td>
+                                        <td>{{quantity}}</td>
+                                        <td><img src="{{img}}" alt="Product Image"></td>
+                                        <td>{{date_added}}</td>
+                                        <td>
+                                            <a href="./inventory/read.php?id={{id}}" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>
+                                            <a href="./inventory/update.php?id={{id}}" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>
+                                            <a href="./inventory/delete.php?id={{id}}" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>
+                                        </td>
+                                    </tr>
+                                ';
+                        
+                                // Populate the rows using the row template
+                                $rows = '';
+                                while ($row = $result->fetch()) {
+                                    $rowHtml = str_replace(
+                                        array('{{id}}', '{{title}}', '{{description}}', '{{price}}', '{{rrp}}', '{{quantity}}', '{{img}}', '{{date_added}}'),
+                                        array($row['id'], $row['title'], $row['description'], $row['price'], $row['rrp'], $row['quantity'], $row['img'], $row['date_added']),
+                                        $rowTemplate
+                                    );
+                                    $rows .= $rowHtml;
+                                }
+                        
+                                // Replace the rows placeholder in the table template with the actual rows
+                                echo str_replace('{{rows}}', $rows, $tableTemplate);
+                                
+                                // Free result set
+                                unset($result);
+                            } else{
+                                echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+                            }
+                        } else{
+                            echo "Oops! Something went wrong. Please try again later.";
+                        }
+                        
+                        // Close connection
+                        unset($pdo);
+                        ?>
+                    </div>
+                </div>        
+            </div>
+        </div>
+    </div>
 </body>
 </html>
