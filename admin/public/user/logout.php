d6851a648947a6dd5f5b6fa7592767644a87ab55
Lord-binns <vince.olemberio2004gmail.com>
2024-05-21 19:38:13 +0800

    fixed the signout page

--- admin/public/user/logout.php
+++ admin/public/user/logout.php
@@ -9,6 +9,6 @@ $_SESSION = array();
 session_destroy();
  
 // Redirect to login page
-header("Location: /olemberio-it28a/admin/index.php");
+header("Location: /olemberio-pdo-crud/admin/index.php");
 exit;
 ?>
