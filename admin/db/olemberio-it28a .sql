ae91521825e020ef2d6f39e9da2ce5aae3a52f33
Lord-binns <vince.olemberio2004gmail.com>
2024-05-21 22:24:44 +0800

    edited my database dump

--- admin/db/olemberio-it28a .sql
+++ admin/db/olemberio-it28a .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2024 at 02:08 PM
+-- Generation Time: May 21, 2024 at 04:22 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -47,7 +47,10 @@ INSERT INTO `products` (`id`, `title`, `description`, `price`, `rrp`, `quantity`
 (2, 'Phoenix Feather Wand', 'Crafted from the feather of a mythical phoenix, this wand channels powerful magical energies and is said to grant the user control over fire and resurrection.', 2800, 2000, 30, 'https://img.freepik.com/premium-photo/players-can-wield-power-phoenix-feather-wand-ai-generated-art_848850-1742.jpg', '2024-05-08 00:00:00'),
 (3, 'Dragon Scale Armor', 'Crafted from the impenetrable scales of a dragon, this armor provides exceptional protection against physical and magical attacks.', 1600, 1900, 40, 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3d57ada8-f751-4596-8fed-58a9cf404347/dfw2bbs-55e6398f-c27d-4ea0-87ed-6c25ca3ee0ba.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzNkNTdhZGE4LWY3NTEtNDU5Ni04ZmVkLTU4YTljZjQwNDM0N1wvZGZ3MmJicy01NWU2Mzk4Zi1jMjdkLTRlYTAtODdlZC02YzI1Y2EzZWUwYmEuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.DJyo094rdy6eZv16BY586qOEwxPRXuEY7VX4m9lYjF4', '2024-05-08 00:00:00'),
 (4, 'Mermaid\'s Tear Necklace', 'A necklace made from the tears of mermaids, rumored to grant the wearer the ability to breathe underwater and communicate with sea creatures.', 1700, 1800, 60, 'https://images.pexels.com/photos/906056/pexels-photo-906056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', '2024-05-08 00:00:00'),
-(5, 'Griffin Claw Dagger', 'A dagger crafted from the claw of a griffin, known for its sharpness and the power to strike with the force of a thunderbolt.', 1000, 900, 20, 'https://pic.ebid.net/upload_big/1/5/1/uo_1703076903-10674-1.jpg', '2024-05-08 00:00:00');
+(5, 'Griffin Claw Dagger', 'A dagger crafted from the claw of a griffin, known for its sharpness and the power to strike with the force of a thunderbolt.', 1000, 900, 20, 'https://pic.ebid.net/upload_big/1/5/1/uo_1703076903-10674-1.jpg', '2024-05-08 00:00:00'),
+(6, 'Dragonfire Sword', 'A sword forged in dragonfire, imbued with the elemental power of flames and capable of igniting foes with a single strike.', 1500, 1600, 50, 'https://imagedelivery.net/9sCnq8t6WEGNay0RAQNdvQ/UUID-cl9hsevsq5654qnoppi6um908/public', '2024-05-21 20:22:45'),
+(7, 'Potion of Invisibility', 'A potion brewed from rare herbs and enchanted to render the drinker invisible for a limited duration, perfect for stealth and evasion.', 1800, 1850, 50, 'https://images.squarespace-cdn.com/content/v1/52c8e848e4b06ad2e570480f/066697b7-74d7-46df-ab0a-13a4b3d64c09/potion+of+invulnerability.png', '2024-05-21 20:41:29'),
+(8, 'Moonlight Blade', 'A blade infused with moonlight essence, granting its wielder heightened agility and the ability to strike with ethereal precision under moonlit skies.', 1700, 1800, 40, 'https://i.etsystatic.com/50930825/r/il/ee61bf/5918960685/il_fullxfull.5918960685_6a80.jpg', '2024-05-21 20:59:15');
 
 -- --------------------------------------------------------
 
@@ -89,7 +92,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
